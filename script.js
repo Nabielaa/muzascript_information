c1e5b10f497283d5410c02ebfbedb4d590a56512
Nbillllla <132736566+Nabielaa@users.noreply.github.com>
2026-01-20 16:05:42 +0700

    script.js

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
 const text = "Hello! welcome to muzascript website";
 const typingText = document.getElementById("typingText");
-const btnAbout = document.getElementById("btnAbout");
-const infoSection = document.getElementById("infoSection");
+const info = document.getElementById("info");
 const intro = document.getElementById("intro");
 const social = document.getElementById("social");
 
 let i = 0;
 
-function type() {
+function typeText() {
   if (i < text.length) {
     typingText.textContent += text.charAt(i);
     i++;
-    setTimeout(type, 40);
-  } else {
-    btnAbout.classList.remove("hidden");
+    setTimeout(typeText, 40);
   }
 }
 
-type();
+typeText();
 
 function showInfo() {
-  btnAbout.classList.add("hidden");
-  infoSection.classList.remove("hidden");
+  info.classList.remove("hidden");
 }
 
 function showPic(n) {
   document.getElementById("pic1").classList.add("hidden");
   document.getElementById("pic2").classList.add("hidden");
 
-  document.getElementById(
-    n === 1 ? "pic1" : "pic2"
-  ).classList.remove("hidden");
+  document.getElementById(n === 1 ? "pic1" : "pic2")
+    .classList.remove("hidden");
 }
 
 function showSocial() {
