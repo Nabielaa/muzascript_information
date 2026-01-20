785b91c85449aec3c86dca6e54e6192d6284c96d
Nbillllla <132736566+Nabielaa@users.noreply.github.com>
2026-01-20 16:19:17 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const text = "Hello! welcome to muzascript website";
+const typingText = document.getElementById("typingText");
+
+let i = 0;
+let speed = 40;
+
+function typeText() {
+  if (i < text.length) {
+    typingText.textContent += text.charAt(i);
+    i++;
+    setTimeout(typeText, speed);
+  }
+}
+
+typeText();
+
 function showAbout() {
   document.getElementById("about").style.display = "block";
 }
