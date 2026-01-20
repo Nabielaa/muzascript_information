3ae7b9cfbabfddf210cda9c498936047f2756982
Nbillllla <132736566+Nabielaa@users.noreply.github.com>
2026-01-20 16:12:45 +0700

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-function showInfo() {
-  document.getElementById("info").style.display = "block";
+function showAbout() {
+  document.getElementById("about").style.display = "block";
 }
 
 function showSocial() {
   document.getElementById("social").style.display = "block";
-}
-
-function showPic(n) {
-  document.getElementById("pic1").style.display = "none";
-  document.getElementById("pic2").style.display = "none";
-
-  if (n === 1) {
-    document.getElementById("pic1").style.display = "block";
-  } else {
-    document.getElementById("pic2").style.display = "block";
-  }
 }
