2da7dea2ef3ad3498fdf175efe2a3ddc2bc26b6f
Nbillllla <132736566+Nabielaa@users.noreply.github.com>
2026-01-20 16:09:05 +0700

    script.js

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
-const text = "Hello! welcome to muzascript website";
-const typingText = document.getElementById("typingText");
-const info = document.getElementById("info");
-const intro = document.getElementById("intro");
-const social = document.getElementById("social");
-
-let i = 0;
-
-function typeText() {
-  if (i < text.length) {
-    typingText.textContent += text.charAt(i);
-    i++;
-    setTimeout(typeText, 40);
-  }
-}
-
-typeText();
-
 function showInfo() {
-  info.classList.remove("hidden");
-}
-
-function showPic(n) {
-  document.getElementById("pic1").classList.add("hidden");
-  document.getElementById("pic2").classList.add("hidden");
-
-  document.getElementById(n === 1 ? "pic1" : "pic2")
-    .classList.remove("hidden");
+  document.getElementById("info").style.display = "block";
 }
 
 function showSocial() {
-  intro.classList.add("hidden");
-  social.classList.remove("hidden");
+  document.getElementById("social").style.display = "block";
+}
+
+function showPic(n) {
+  document.getElementById("pic1").style.display = "none";
+  document.getElementById("pic2").style.display = "none";
+
+  if (n === 1) {
+    document.getElementById("pic1").style.display = "block";
+  } else {
+    document.getElementById("pic2").style.display = "block";
+  }
 }
